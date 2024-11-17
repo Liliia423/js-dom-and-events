77c73338decda29e9675b318da0963133d1dc3cb
Liliia423 <etude4408@gmail.com>
2024-11-17 13:31:52 +0200

    rty

--- js/task-3.js
+++ js/task-3.js
@@ -1,46 +1,44 @@
-const users = [
-  {
-    name: 'Moore Hensley',
-    friends: ['Sharron Pace'],
-    gender: 'male',
-  },
-  {
-    name: 'Sharlene Bush',
-    friends: ['Briana Decker', 'Sharron Pace'],
-    gender: 'female',
-  },
-  {
-    name: 'Ross Vazquez',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    gender: 'male',
-  },
-  {
-    name: 'Elma Head',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-    gender: 'female',
-  },
-  {
-    name: 'Carey Barr',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-    gender: 'male',
-  },
-  {
-    name: 'Blackburn Dotson',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    gender: 'male',
-  },
-  {
-    name: 'Sheree Anthony',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-    gender: 'female',
-  },
-];
-
-const friendsCounts = users.map(user => ({
-  name: user.name,
-  friendsCount: user.friends.length,
-}));
-
-const sortedByDescentingRating = users.toSorted((firstFriend, secondFriend) => secondFriend.friendsCounts - firstFriend.friendsCounts);
-
-console.log(sortedByDescentingRating);
+const sortByDescendingFriendCount = users => {
+  const lengthFtiendsArray = users.friends.length;
+  const arraySortByCountFriend = lengthFtiendsArray.toSorted((a, b) => b - a);
+  return arraySortByCountFriend;
+};
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
+    },
+    {
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
+    },
+    {
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
+    },
+    {
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
+    },
+    {
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
+    },
+    {
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
+    },
+    {
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
+  ])
+);
