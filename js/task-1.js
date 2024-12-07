dd467bbaa705b6cc724efec4ab0e23fd01f22dae
Liliia423 <etude4408@gmail.com>
2024-12-07 20:57:57 +0200

    ty

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,17 @@
-//const numberOfCategories = classList.length;
-const category = document.querySelectorAll('.item');
-//console.log(category);
-console.log('Number of categories: ', category.length);
-//Number of categories: 3
+const categories = document.querySelectorAll('.item');
+//console.log(categories); //вийшов масив категорій "тег.назва класу" li.item
+console.log('Number of categories: ', categories.length);
+
+const headerOfList = document.querySelectorAll('#categories .item h2'); // Тепер це список всіх h2
+const listInsideCategory = document.querySelectorAll('#categories .item ul');
+
+headerOfList.forEach(header => {
+  console.log(`Category: ${header.textContent}`);
+  console.log(`Elements: ${listInsideCategory.length}`);
+});
+//console.log(headerOfList.classList.contains('header')); //перевірили header-клас
+
+//
 //Category: Animals
 //Elements: 4
 //Category: Products
@@ -13,6 +22,7 @@ console.log('Number of categories: ', category.length);
 //З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
 //Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+//+ Number of categories: 3
 //Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
 //На що буде звертати увагу ментор при перевірці:
