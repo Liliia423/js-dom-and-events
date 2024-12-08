91fafd609069bc3e1e10edded568ccc2059b2250
Liliia423 <etude4408@gmail.com>
2024-12-08 19:01:04 +0200

    rty

--- js/task-5.js
+++ js/task-5.js
@@ -1,9 +1,14 @@
 const changeBackgroundColorBtn = document.querySelector('button.change-color');
-
-const bodyStyle = body.classList.add('body-style');
+const body = document.querySelector('body');
+const spanColorValue = document.querySelector('span.color');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+changeBackgroundColorBtn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColorValue.textContent = randomColor;
+});
