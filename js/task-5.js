29392208d9e44bd66a58b0c285c5b7dcb2bd5ab0
Liliia423 <etude4408@gmail.com>
2024-12-08 18:31:22 +0200

    rty

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,7 @@
+const changeBackgroundColorBtn = document.querySelector('button.change-color');
+
+const bodyStyle = body.classList.add('body-style');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
