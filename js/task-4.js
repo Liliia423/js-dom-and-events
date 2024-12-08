08f9134f3e7533f814354f2ce8152d51f3fe9bbb
Liliia423 <etude4408@gmail.com>
2024-12-08 18:01:33 +0200

    rty

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    alert('All form fields must be filled in');
+    return;
+  }
+  const validOutputForm = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  console.log(validOutputForm);
+
+  form.reset();
+});
